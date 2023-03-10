d0a9f881376750eb700d91ee36d1987c76c468ca
Anton1Ilyin <ilin.ai@phystech.edu>
2023-03-10 13:11:03 +0300

    geters

--- Classes/parachuteInterface.cpp
+++ Classes/parachuteInterface.cpp
@@ -5,3 +5,51 @@ void parachuteInterface::itWorks()
 {
     cout<<"It works!"<<endl;
 }
+string get_title
+{
+    return title;
+}
+float get_area
+{
+    return area;
+}
+float get_weight
+{
+    return weight;
+}
+float get_des_speed
+{
+    return des_speed;
+}
+int get_min_height
+{
+    return min_height;
+}
+bool get_is_main
+{
+    return is_main;
+}
+bool get_forced_only
+{
+    return forced_only;
+}
+float get_speed
+{
+    return speed;
+}
+int get_round_time
+{
+    return round_time;
+}
+bool get_has_extra_safety
+{
+    return has_extra_safety;
+}
+bool get_manual_only
+{
+    return manual_only;
+}
+bool get_has_spring
+{
+    return has_spring;
+}
--- Headers/parachuteInterface.hpp
+++ Headers/parachuteInterface.hpp
@@ -3,7 +3,25 @@
 class parachuteInterface
 {
 public:
+<<<<<<< Updated upstream
     void itWorks();
+=======
+    parachute();
+    virtual ~parachute();
+    void help();
+    string get_title;
+    float get_area;
+    float get_weight;
+    float get_des_speed;
+    int get_min_height;
+    bool get_is_main;
+    bool get_forced_only;
+    float get_speed;
+    int get_round_time;
+    bool get_has_extra_safety;
+    bool get_manual_only;
+    bool get_has_spring;
+>>>>>>> Stashed changes
 protected:
     string title;
 };
